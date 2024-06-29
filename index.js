7dc9f7bc71db7fd986d71500dbdff31d024aa768
WassimMgg <meguellatiwassim1@gmail.com>
2024-06-29 14:51:39 +0100

    fix the bugs and improve the user exprience (check if this country exist or not , then check if this country is  visited or not and return an error message

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 
+
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "World",
   password: "29052003",
-  port: 5433,
+  port: "5433",
 });
 
 db.connect((err) => {
@@ -18,10 +19,19 @@ db.connect((err) => {
   }
 });
 
-async function checkVisisted() {
-  const result = await db.query("SELECT country_code FROM visited_countries");
+async function isVisited(code) {
+  const results = await checkVisited();
+  for (let i = 0; i < results.length; i++) {
+    if (results[i] === code) {
+      return true;
+    }
+  }
+  return false;
+}
 
-  let countries = result.rows.map(row => row.country_code); 
+async function checkVisited() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = result.rows.map(row => row.country_code);
   return countries;
 }
 
@@ -32,25 +42,33 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  const result = await checkVisisted(); 
+  const result = await checkVisited();
+  console.log(result);
   res.render("index.ejs", { countries: result, total: result.length });
 });
 
 app.post("/add", async (req, res) => {
   let country = req.body.country;
   const results = await db.query("SELECT country_code  FROM countries Where country_name = $1", [country]);
-  console.log(results);
 
-  if (results.rows.length !== 0) {
-    const data = results.rows[0];
-    const countryCode = data.country_code;
+  if (!results.rows.length) {
+    const result = await checkVisited();
+    res.render("index.ejs", { error: "Country doesn't exist, try again", countries: result, total: result.length });
+    return;
+  }
 
+  const data = results.rows[0];
+  const countryCode = data.country_code;
+  let visited = await isVisited(countryCode);
+  if (!visited) {
     await db.query(`INSERT INTO visited_countries (country_code) VALUES ('${countryCode}')`);
     res.redirect("/");
+  } else {
+    const result = await checkVisited();
+    res.render("index.ejs", { error: "Country already visited", countries: result, total: result.length });
   }
 });
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
