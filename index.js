676b6bab2124377d4b9e5f9de8f056bf92e1686e
WassimMgg <meguellatiwassim1@gmail.com>
2024-06-29 15:15:09 +0100

    handle the case if the user input a Lower or upper case letters

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get("/", async (req, res) => {
 
 app.post("/add", async (req, res) => {
   let country = req.body.country;
-  const results = await db.query("SELECT country_code FROM countries WHERE country_name LIKE $1", [country + '%']);
+  const results = await db.query("SELECT country_code FROM countries WHERE LOWER(country_name) LIKE $1", [ '%'+ country.toLowerCase() + '%']);
   if (!results.rows.length) {
     const result = await checkVisited();
     res.render("index.ejs", { error: "Country doesn't exist, try again", countries: result, total: result.length });
